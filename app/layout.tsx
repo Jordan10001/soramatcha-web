4bf91d83f2721f9086e3a88a851fbbda47b75d64
Jordan <Jordan10001@users.noreply.github.com>
2025-12-02 11:10:46 +0700

    seo geo

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,25 @@ const defaultUrl = process.env.VERCEL_URL
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Soramatcha",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  description: "The name Sora Matcha is inspired by the Japanese word “sora” (空), which means “sky.” Just like the sky, Sora Matcha represents clarity, calm, and endless possibilities. It reflects our vision of creating a matcha experience that feels light, pure, and intentional—something that lifts your day, grounds your spirit, and gives you a moment to breathe. Every cup is a reminder to slow down and savor simplicity, crafted with the finest ceremonial grade matcha and a personal touch that makes it uniquely ours.",
+  robots: "index, follow",
+  openGraph: {
+    title: "Soramatcha",
+    description:
+      "The name Sora Matcha is inspired by the Japanese word “sora” (空), which means “sky.” Just like the sky, Sora Matcha represents clarity, calm, and endless possibilities. It reflects our vision of creating a matcha experience that feels light, pure, and intentional—something that lifts your day, grounds your spirit, and gives you a moment to breathe. Every cup is a reminder to slow down and savor simplicity, crafted with the finest ceremonial grade matcha and a personal touch that makes it uniquely ours.",
+    url: defaultUrl,
+    siteName: "Soramatcha",
+    images: [
+      {
+        url: `${defaultUrl}/og.png`,
+        width: 1200,
+        height: 630,
+        alt: "Soramatcha",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
 };
 
 const garet = localFont({
